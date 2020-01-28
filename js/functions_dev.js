d499d66c21ff6f90b15b516eb7f71a5ff4ebe985
Davis980520 <davis980520@hotmail.com>
2020-01-28 22:01:56 +0800

    commit

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -103,21 +103,12 @@ function timeElapse(){
    	var minutes = parseInt((difftime % 3600) / 60);
    	var seconds = parseInt(difftime % 60);
 	
-	// var current = new Date();
-	// var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	// var days = Math.floor(seconds / (3600 * 24)) + 31;
-	// seconds = seconds % (3600 * 24);
-	// var hours = Math.floor(seconds / 3600);
-	
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
